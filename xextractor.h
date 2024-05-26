30662c27854cecd21c185354f125b99cb6a74aa5
horsicq <horsicq@gmail.com>
2024-05-26 13:59:59 +0200

    Update file(s): 2024-05-26

--- xextractor.h
+++ xextractor.h
@@ -33,7 +33,7 @@ public:
         QList<XBinary::FT> listFileTypes;
         bool bDeepScan;
         bool bHeuristicScan;
-        bool bMenu_Hex; // For Widget
+        bool bMenu_Hex;  // For Widget
     };
 
     struct RECORD {
