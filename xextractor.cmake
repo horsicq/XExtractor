41150670c570c9748724ad47ad875f9be2c4caee
horsicq <horsicq@gmail.com>
2024-07-09 20:57:07 +0200

    Update file(s): 2024-07-09

--- xextractor.cmake
+++ xextractor.cmake
@@ -1,9 +1,9 @@
 include_directories(${CMAKE_CURRENT_LIST_DIR})
 
-include(${CMAKE_CURRENT_LIST_DIR}/../Formats/xbinary.cmake)
+include(${CMAKE_CURRENT_LIST_DIR}/../Formats/xformats.cmake)
 
 set(XEXTRACTOR_SOURCES
-    ${XBINARY_SOURCES}
+    ${XFORMATS_SOURCES}
     ${CMAKE_CURRENT_LIST_DIR}/xextractor.cpp
     ${CMAKE_CURRENT_LIST_DIR}/xextractor.h
 )
